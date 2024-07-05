3ee70ba3bd774fbe3e3050374048ed3258274882
XE203A <165396733+XE203A@users.noreply.github.com>
2024-07-05 10:29:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "254739937062,254710772666";
 global.read_status = process.env.AUTO_READ_STATUS || "true";
-global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "true";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "254739937062,254710772666";
 
